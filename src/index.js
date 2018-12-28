fba516fa7666945f663a3f7b66912ab63a9ad027
siddhant aher <siaher@cisco.com>
2018-12-27 18:18:04 -0800

    srcfolder

---
+++ src/index.js
@@ -0,0 +1,25 @@
+//Import react and reactDOM 
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+//Create react components
+
+const App= () =>{
+
+    const label='Enter Name'
+
+
+    return <div>
+        <label class='name'>{label}</label>
+        <input id='name' type='text'/>
+        <button style={{color:'white',backgroundColor:'blue'}}>Submit</button>
+    </div>;
+};
+
+
+//taking reac compnents and isplay it in the browser
+
+ReactDOM.render(
+    <App />,
+    document.querySelector('#root'));
